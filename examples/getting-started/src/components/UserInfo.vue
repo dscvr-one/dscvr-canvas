3cba1c111d1c5b19abdb09f3cd3085532142dcaa
Jimmy Madrigal <jimmymadrigal+github@gmail.com>
2024-07-16 09:46:55 -0600

    feat: getting started example
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>

---
+++ examples/getting-started/src/components/UserInfo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { CanvasInterface } from '@dscvr-one/canvas-client-sdk'
+defineProps<{
+  user: CanvasInterface.Lifecycle.User
+}>()
+
+defineEmits<{
+  (e: 'open'): void
+}>()
+</script>
+
+<template>
+  <a class="flex gap-4 group hover:text-indigo-400 hover:cursor-pointer" @click="$emit('open')">
+    <img v-if="user.avatar" :src="user.avatar" alt="User Avatar" class="size-20 rounded-full" />
+
+    <div class="flex flex-col gap-1">
+      <h1 class="text-lg font-bold">User Info</h1>
+      <p>
+        <b>User ID: </b><span class="text-gray-300 group-hover:text-indigo-300">{{ user.id }}</span>
+      </p>
+      <p>
+        <b>User Name: </b
+        ><span class="text-gray-300 group-hover:text-indigo-300">{{ user.username }}</span>
+      </p>
+    </div>
+  </a>
+</template>

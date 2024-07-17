76faa6486d55e5a6ccbe441ca0f71f57cd4d1307
Jimmy Madrigal Cruz <jimmymadrigal+github@gmail.com>
2024-07-17 16:19:16 -0600

    feat: getting started example (DCP-2034) (#4)
    * feat: getting started example
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>
    
    * feat: getting started example
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>
    
    * refactor: add semicolon
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>
    
    * fix: pr feedback
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>
    
    ---------
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>

---
+++ examples/getting-started/src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+import { CanvasInterface, CanvasClient } from '@dscvr-one/canvas-client-sdk';
+import UserInfo from './components/UserInfo.vue';
+import ContentInfo from './components/ContentInfo.vue';
+import { validateHostMessage } from './api/dscvr';
+
+let canvasClient: CanvasClient | undefined;
+const isReady = ref(false);
+const user = ref<CanvasInterface.Lifecycle.User>();
+const content = ref<CanvasInterface.Lifecycle.Content>();
+
+const start = async () => {
+  if (!canvasClient) return;
+  const response = await canvasClient.ready();
+  const isValidResponse = await validateHostMessage(response);
+  if (!isValidResponse) return;
+  isReady.value = canvasClient.isReady;
+  if (response) {
+    user.value = response.untrusted.user;
+    content.value = response.untrusted.content;
+  }
+};
+
+const openUserProfile = () => {
+  if (!canvasClient || !user.value) return;
+  const url = `https://dscvr.one/u/${user.value.username}`;
+  canvasClient.openLink(url);
+};
+
+onMounted(() => {
+  canvasClient = new CanvasClient();
+  start();
+});
+
+onUnmounted(() => {
+  if (canvasClient) {
+    canvasClient.destroy();
+  }
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-6 p-8">
+    <p v-if="!isReady" class="text-center">Loading...</p>
+    <template v-else>
+      <user-info v-if="user" :user="user" @open="openUserProfile" />
+      <content-info v-if="content" :content="content" />
+    </template>
+  </div>
+</template>
---
+++ examples/getting-started/src/components/ContentInfo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { CanvasInterface } from '@dscvr-one/canvas-client-sdk';
+defineProps<{
+  content: CanvasInterface.Lifecycle.Content;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <h1 class="text-lg font-bold">Content Info</h1>
+    <p>
+      <b>ID:</b> <span class="text-gray-300">{{ content.id }}</span>
+    </p>
+    <template v-if="content.portalId">
+      <p>
+        <b>Portal Id: </b> <span class="text-gray-300">{{ content.portalId }}</span>
+      </p>
+      <p>
+        <b>Portal Name: </b> <span class="text-gray-300">{{ content.portalName }}</span>
+      </p>
+    </template>
+  </div>
+</template>

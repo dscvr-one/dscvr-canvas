ed14a383396a2aa74570ddba635b9503fa4654b1
Jimmy Madrigal Cruz <jimmymadrigal+github@gmail.com>
2024-07-17 16:28:01 -0600

    feat: transaction example (DCP-2035) (#5)
    * feat: transaction example
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>
    
    * refactor: add semicolon
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>
    
    ---------
    
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>

---
+++ examples/transaction/src/api/dscvr.ts
@@ -0,0 +1,6 @@
+import type { CanvasInterface } from '@dscvr-one/canvas-client-sdk';
+
+export const validateHostMessage = async <T>(message: CanvasInterface.HostMessage) => {
+  // TODO: connect with graphql
+  return true;
+};
---
+++ examples/transaction/src/api/solana.ts
@@ -0,0 +1,47 @@
+import {
+  Connection,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionMessage,
+  VersionedTransaction,
+  clusterApiUrl,
+  type Cluster
+} from '@solana/web3.js';
+
+export const getTransactionDetails = (cluster: Cluster, transactionId: string) => {
+  const connection = new Connection(clusterApiUrl(cluster));
+  return connection.getParsedTransaction(transactionId, {
+    commitment: 'confirmed',
+    maxSupportedTransactionVersion: 0
+  });
+};
+
+export const createSendSolTransaction = async (
+  cluster: Cluster,
+  amount: number,
+  fromPubKey: PublicKey,
+  toPubKey: PublicKey,
+  appUrl?: string
+) => {
+  const connectionEndpoint = appUrl ?? clusterApiUrl(cluster);
+  const connection = new Connection(connectionEndpoint);
+  const blockhash = await connection.getLatestBlockhash().then((res) => res.blockhash);
+
+  const tx = new Transaction();
+
+  tx.add(
+    SystemProgram.transfer({
+      fromPubkey: fromPubKey,
+      toPubkey: toPubKey,
+      lamports: amount * 10 ** 9
+    })
+  );
+  const messageV0 = new TransactionMessage({
+    payerKey: fromPubKey,
+    recentBlockhash: blockhash,
+    instructions: tx.instructions
+  }).compileToV0Message();
+
+  return new VersionedTransaction(messageV0);
+};

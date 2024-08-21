69ca53f5baad96a66719e86af461676669fcff91
Jimmy Madrigal <jimmymadrigal+github@gmail.com>
2024-08-21 09:28:51 -0600

    fix: canvas wallet registration
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>

--- libs/canvas-wallet-adapter/src/register.ts
+++ libs/canvas-wallet-adapter/src/register.ts
@@ -1,13 +1,19 @@
 import { getWallets } from '@wallet-standard/core';
 import { registerWallet } from '@wallet-standard/wallet';
 import type { CanvasClient } from '@dscvr-one/canvas-client-sdk';
-import { CanvasWallet } from './wallet';
+import { CANVAS_WALLET_NAME, CanvasWallet } from './wallet';
 
 export const registerCanvasWallet = (canvasClient: CanvasClient) => {
   const { get } = getWallets();
   const wallets = get();
-  const exists = wallets.find((wallet) => wallet.name === CanvasWallet.Name);
-  if (exists) return exists;
+  const exists = wallets.find((wallet) => wallet.name === CANVAS_WALLET_NAME);
+  if (exists) {
+    // TODO: Since the user could potentially instantiate the canvas client more than once
+    // we need to update the canvas client in the wallet
+    // A common scneario is React testing environment that calls hooks twice.
+    (exists as CanvasWallet).setCanvasClient(canvasClient);
+    return exists;
+  }
 
   const wallet = new CanvasWallet(canvasClient);
   registerWallet(wallet);

8e4559f87ae1f294c2821a7086c8e562aaf8db40
Chandra Penke <chandrapenke@gmail.com>
2024-11-15 17:04:47 -0800

    fix: removing shipjs (#14)

--- libs/canvas-client-sdk/ship.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  installCommand: () => 'npm ci --prefer-offline --no-audit',
-  buildCommand: () => 'npm run build',
-  publishCommand: ({ defaultCommand, tag }) =>
-    `${defaultCommand} --access public`,
-};
